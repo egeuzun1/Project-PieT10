34149aa1aa69f401bb9a811f4bf1f00e7082b9a6
şule yıldırım <19061038@stu.omu.edu.tr>
2024-08-08 23:24:01 +0300

    update

--- function.py
+++ function.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-import pandas as pd
-import random as rand
-
-
-data=pd.read_csv("main.csv",encoding="ISO-8859-1")
-second_row=data.iloc[1]
-a=rand.choice(second_row)
-
-print(a)
-=======
 
 import numpy as np
 import pandas as pd
@@ -17,9 +5,28 @@ import random as rand
 import csv 
 
 
-data=pd.read_csv("imdb1000.csv",encoding="ISO-8859-1", on_bad_lines='skip')
+movie=pd.read_csv("imdb1000.csv",encoding="ISO-8859-1", on_bad_lines='skip')
 a=rand.randint(1,900)
-film1=data.loc[a][1]
+b=rand.randint(1,900)
 
-print(film1)
->>>>>>> 0261c0e1f7a506d69f2bf67bcaa9af99b08ff153
+class movie1:
+    #Poster linkini ekleyemedik:()
+    name=movie.loc[a]["name"]
+    year=movie.loc[a]["year"]
+    
+
+class movie2:
+    #Poster linkini ekleyemedik:(
+    name=movie.loc[b]["name"]
+    year=movie.loc[b]["year"]
+
+
+def game(movie1,movie2):
+    print(movie1,"vs",)
+
+
+       
+
+
+
+
