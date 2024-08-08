0261c0e1f7a506d69f2bf67bcaa9af99b08ff153
şule yıldırım <19061038@stu.omu.edu.tr>
2024-08-08 22:37:08 +0300

    update

--- function.py
+++ function.py
@@ -2,10 +2,11 @@
 import numpy as np
 import pandas as pd
 import random as rand
+import csv 
 
 
-data=pd.read_csv("main.csv",encoding="ISO-8859-1", on_bad_lines='skip')
-second_row=data.iloc[1]
-a=rand.choice(second_row)
+data=pd.read_csv("imdb1000.csv",encoding="ISO-8859-1", on_bad_lines='skip')
+a=rand.randint(1,900)
+film1=data.loc[a][1]
 
-print(a)
+print(film1)
