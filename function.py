12e37964a6e3c161fd72e4017122a940b30d0378
şule yıldırım <19061038@stu.omu.edu.tr>
2024-08-09 19:49:02 +0300

    update

--- function.py
+++ function.py
@@ -3,8 +3,8 @@ import random as rand
 
 
 movie=pd.read_csv("imdb1000.csv",encoding="ISO-8859-1", on_bad_lines='skip')
-a=rand.randint(1,900)
-b=rand.randint(1,900)
+a=rand.randint(1,985)
+b=rand.randint(1,985)
 
 can_sayisi=3
 skor=0
@@ -20,25 +20,38 @@ class movie2:
     name=movie.loc[b]["name"]
     year=movie.loc[b]["year"]
 
+print("")
 
 def game():
     global can_sayisi
     global skor
-    print(movie1.name,"vs",movie2.name)
-    guess=int(input("Hangisi daha önce çıkan bir filmdir?(1. Film için 1, ikincisi için 2 yazınız.)")) #TODO: tıklayarak nasıl olacak, tryexcept kullanarak
-    def answer():
-        if movie1.year<movie2.year:
-            return 1
-        else:
-            return 2
-    if guess==answer():
-        print("Doğru!\n+1 Puan")
-        skor+=1
-        print(movie1.year,movie2.year)
-        print("Skor:",skor,"Can Sayısı:",can_sayisi)
-        #TODO:SKOR VE CAN HER ZAMAN SAĞ ÜSTTE NASIL YAZACAK
-    elif guess!=answer:
-        print("Yanlış!\n1 Can Kaybettin")
-        can_sayisi-=1
-        print("Skor:",skor,"Can Sayısı:",can_sayisi)
+    global a
+    global b
+    while can_sayisi>0:
+        a=rand.randint(1,985)
+        print(movie1.name,"vs",movie2.name)
+        guess=int(input("Hangisi daha önce çıkan bir filmdir?(1. Film için 1, ikincisi için 2 yazınız.)")) #TODO: tıklayarak nasıl olacak, tryexcept kullanarak
+        def answer():
+            if movie1.year<movie2.year:
+                return 1
+            else:
+                return 2
+        if guess==answer():
+            print("Doğru!\n+1 Puan")
+            skor+=1
+            print(movie1.year,movie2.year)
+            print("Skor:",skor,"Can Sayısı:",can_sayisi)
+            #TODO:SKOR VE CAN HER ZAMAN SAĞ ÜSTTE NASIL YAZACAK
+        elif guess!=answer:
+            print("Yanlış!\n1 Can Kaybettin")
+            can_sayisi-=1
+            print("Skor:",skor,"Can Sayısı:",can_sayisi)
+       
+        a=b
+        b=rand.randint(1,985)
+    if can_sayisi==0:
+        print("Ha ha öldün çık:)")
+#10 25 50 100 250 500 980
+        
 game()
+
